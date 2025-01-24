8d4395342cfa069c8bf016cc568109c8529d3fb6
gg307 <gg307@email.com>
2025-01-24 23:24:35 +0100

    add

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,75 @@
 
 namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& v, int n)
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			v.push_back(i * i);
+		}
+	}
+
+	void remove_element(std::vector<int>& v, int i)
+	{
+		v.erase(v.begin() + i);
+	}
+
+	void input_element(std::vector<std::string>& v, int i, std::string s)
+	{
+		v.insert(v.begin() + i, s);
+	}
+
+	unsigned unique_numbers(std::istream& is)
+	{
+		std::set<int> set{};
+		int n;
+		while (is >> n)
+		{
+			set.insert(n);
+		}
+		return set.size();
+	}
+
+	word_frequency::word_frequency(std::istream& is)
+	{
+		std::string s;
+		while (is >> s)
+		{
+			s[0] = tolower(s[0]);
+			insert(s);
+		}
+	}
+
+	void word_frequency::insert(const std::string& s)
+	{
+		++map[s];
+	}
+
+	unsigned word_frequency::count()
+	{
+		return map.size();
+	}
+
+	unsigned word_frequency::frequency(std::string s)
+	{
+		auto it = map.find(s);
+		if (it != map.end())
+		{
+			return map.at(s);
+		}
+		return 0;
+	}
+
+	int list_nth_element(std::list<int>& l, int i)
+	{
+		auto it = l.begin();
+		std::advance(it, i);
+		return *it;
+	}
+
+	void list_sort_desc(std::list<int>& l)
+	{
+		l.sort(std::greater<int>());
+	}
 
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,28 @@
 #pragma once
 
-namespace vsite::oop::v9 
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <list>
+
+namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& v, int n);
+	void remove_element(std::vector<int>& v, int i);
+	void input_element(std::vector<std::string>& v, int i, std::string s);
+	int list_nth_element(std::list<int>& l, int i);
+	void list_sort_desc(std::list<int>& l);
+	unsigned unique_numbers(std::istream& is);
+
+	class word_frequency
+	{
+	private:
+		std::map<std::string, unsigned> map;
+	public:
+		word_frequency(std::istream& is);
+		void insert(const std::string& s);
+		unsigned count();
+		unsigned frequency(std::string s);
+	};
 }
